5a994bd7bce00da3aba3f7fa5318f724d93fd7b2
maatthw <displead1@gmail.com>
2026-07-10 00:26:12 -0400

    better explanation

--- 0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters.py
@@ -18,4 +18,4 @@ def lengthOfLongestSubstring(s):
     # Character set could be all lowercase letters, all uppercase and lowercase, all ASCII, all Unicode (1m+), a combination, or god knows what else
     # Based on the constraint of this problem: "s consists of English letters, digits, symbols, and spaces", it is reasonable to assume a character set of 128 (ASCII)
     # Thus, the space complexity would really be O(min(n, 128)) which resolves to O(1) since for large values of n our function never exceeds 128
-    # O(1) would be the more "precise" answer given the constraints, but O(min(n, m)) is a more general answer for broader constraints that could be applied to this problem
+    # O(1) would be the more "precise" answer given the constraints, but O(min(n, m)) is a more general answer that covers a wider range of character set constraint variations
